a8378269709971ac97bc10a8f987b73f3032cd52
md1300 <md.tawhid.khan1998@gmail.com>
2025-09-22 00:21:53 +0600

    include github log in

---
+++ src/utils/authOptions.ts
@@ -0,0 +1,16 @@
+import { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+
+
+export const authOptions:NextAuthOptions= {
+  secret: process.env.NEXTAUTH_SECRET,
+  // Configure one or more authentication providers
+  providers: [
+    GitHubProvider({
+    clientId: process.env.GITHUB_ID as string,
+    clientSecret: process.env.GITHUB_SECRET as string
+  }),
+  ],
+  
+}
+
