546793177a77f6eaa8dd7162dae7b8f9f7cf2d49
md1300 <md.tawhid.khan1998@gmail.com>
2025-09-22 12:52:45 +0600

    create middleware to protect specific route

--- src/utils/authOptions.ts
+++ src/utils/authOptions.ts
@@ -4,6 +4,9 @@ import GoogleProvider from "next-auth/providers/google"
 
 
 export const authOptions:NextAuthOptions= {
+  pages:{
+    signIn:"/login"
+  },
   secret: process.env.NEXTAUTH_SECRET,
   // Configure one or more authentication providers
   providers: [
