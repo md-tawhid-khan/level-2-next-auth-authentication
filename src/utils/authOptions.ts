a4617676c6c1d1b9e2d923baf529f66c1bde7ce1
md1300 <md.tawhid.khan1998@gmail.com>
2025-09-22 12:32:31 +0600

    add google log in

--- src/utils/authOptions.ts
+++ src/utils/authOptions.ts
@@ -1,5 +1,6 @@
 import { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google"
 
 
 export const authOptions:NextAuthOptions= {
@@ -10,7 +11,11 @@ export const authOptions:NextAuthOptions= {
     clientId: process.env.GITHUB_ID as string,
     clientSecret: process.env.GITHUB_SECRET as string
   }),
-  
+    GoogleProvider({
+    clientId: process.env.GOOGLE_ID as string,
+    clientSecret: process.env.GOOGLE_SECRET as string
+  }),
+
   ],
   
 }
